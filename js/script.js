44304a3059c2c05b177d4e50e9f38534ab22cfab
allanfpcruz <137228336+allanfpcruz@users.noreply.github.com>
2024-02-13 17:41:16 -0300

    aplicação da tranformação no elemento

--- js/script.js
+++ js/script.js
@@ -5,13 +5,15 @@ const element = document.querySelector('#element')
 const codeArea = document.querySelector('#code-area')
 
 //funções
+
+//cria o código de tranformação
 function generateTransform(value, transform) {
   let measure = ''
 
   if(transform.includes('translate')) {
     measure = 'px'
-  } else if (transform.includes('rotate')) {
-    measure = '°'
+  } else if (transform.includes('rotate') || transform.includes('skew')) {
+    measure = 'deg'
   } else {
     measure = ''
   }
@@ -19,16 +21,29 @@ function generateTransform(value, transform) {
   let code = `${transform}(${value}${measure})`
   console.log(code)
 
-  if (!codeArea.value.includes(transform) || !codeArea.value.includes('none')) {
-    if (codeArea.value.includes(transform.slice(0, transform.length-2))) {
+  applyTransform(code)
 
-    } else {
-      codeArea.value = `${codeArea.value.slice(0, codeArea.value.length - 1)}, ${code};`
-    }
-  } else {
-    codeArea.value = `transform: ${code};`
-  }
 }
+
+// aplica o código no elemento
+function applyTransform(code) {
+
+  element.style.transform = code
+
+}
+
+//esboço do código
+
+//  if (!codeArea.value.includes(transform) || !codeArea.value.includes('none')) {
+//    if (codeArea.value.includes(transform.slice(0, transform.length-2))) {
+//      codeArea.value = `${codeArea.value.slice()}`
+//    } else {
+//      codeArea.value = `${codeArea.value.slice(0, codeArea.value.length - 1)}, ${code};`
+//    }
+//  } else {
+//    codeArea.value = `transform: ${code};`
+//  }
+
 
 //eventos
 inputs.forEach(input => {
