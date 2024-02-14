38d08bd32a3733bc00a6caa9aabb3f068f05b514
allanfpcruz <137228336+allanfpcruz@users.noreply.github.com>
2024-02-14 18:59:49 -0300

    mais algumas funcionalidades

--- js/script.js
+++ js/script.js
@@ -7,9 +7,40 @@ const codeArea = document.querySelector('#code-area')
 //funções
 
 //cria o código de tranformação
-function generateTransform(value, transform) {
-  let measure = ''
+function generateTransform(value, transform, box) {
 
+  let measure = setMeasure(transform)
+
+  let code = `${transform}(${value}${measure})`
+  console.log(code)
+
+  applyTransform(code, transform)
+
+  //coloca o número no índice
+  box.innerHTML = value + measure
+
+}
+
+// aplica o código no elemento
+function applyTransform(code, transform) {
+
+  if (element.style.transform.length < code.length) {
+    element.style.transform = code
+  } else if (!element.style.transform.includes(transform)) {
+    element.style.transform += code
+  } else {
+
+  }
+
+}
+
+//coloca o número do transform na interface
+
+
+//define a unidade de medida
+function setMeasure(transform) {
+
+  let measure = ''
   if(transform.includes('translate')) {
     measure = 'px'
   } else if (transform.includes('rotate') || transform.includes('skew')) {
@@ -18,17 +49,7 @@ function generateTransform(value, transform) {
     measure = ''
   }
 
-  let code = `${transform}(${value}${measure})`
-  console.log(code)
-
-  applyTransform(code)
-
-}
-
-// aplica o código no elemento
-function applyTransform(code) {
-
-  element.style.transform = code
+  return measure
 
 }
 
@@ -44,11 +65,17 @@ function applyTransform(code) {
 //    codeArea.value = `transform: ${code};`
 //  }
 
-
 //eventos
 inputs.forEach(input => {
   input.addEventListener('change', (e) => {
     let transformType = input.getAttribute('name')
-    generateTransform(input.value, transformType)
+    let numBox = input.parentNode.querySelector('.control-num')
+    generateTransform(input.value, transformType, numBox)
+
+    //teste
+    let phrase = 'banana com granola'
+    let word = 'com'
+    let res = phrase.slice(0, phrase.lastIndexOf(word))
+    console.log(res)  
   })
 })
